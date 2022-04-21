d2ac9595141a2ba59cb43527d3e2b8373a54b112
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-04-21 16:52:39 -0400

    Requirement 5 complete! :heavy_check_mark: Search Page created

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.onChange = this.onChange.bind(this);
+
+    this.state = {
+      disabled: true,
+    };
+  }
+
+  onChange({ target }) {
+    const minCharacter = 2;
+    const { value } = target;
+    if (value.length >= minCharacter) this.setState({ disabled: false });
+    else this.setState({ disabled: true });
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            placeholder="Nome da banda ou artista"
+            onChange={ this.onChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ disabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
